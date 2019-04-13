028147b234361d227f02ddb34b3cc3b186163ea6
Sonny Kothapally <me@sonnyksimon.com>
2019-04-13 13:42:51 -0400

    use static.ren.pancubs.org

--- schema.py
+++ schema.py
@@ -2,36 +2,34 @@ from settings import db
 import peewee as sql
 
 class Table(sql.Model):
-	class Meta:
-		database = db
+    class Meta:
+        database = db
 
 class User(Table):
-	name = sql.CharField()
-	password = sql.CharField()
-	verified = sql.BooleanField()
+    name = sql.CharField()
+    password = sql.CharField()
+    verified = sql.BooleanField()
 
 class Thing(Table):
-	key = sql.CharField()
+    key = sql.CharField()
 
 class Version(Table):
-	revision = sql.IntegerField()
-	thing_id = sql.ForeignKeyField(Thing, backref='versions')
-	author_id = sql.ForeignKeyField(User, backref='versions')
-	ip = sql.CharField() # should use sql.IPField()
-	comment = sql.CharField()
-	created = sql.TimestampField()
+    revision = sql.IntegerField()
+    thing_id = sql.ForeignKeyField(Thing, backref='versions')
+    author_id = sql.ForeignKeyField(User, backref='versions')
+    ip = sql.CharField() # should use sql.IPField()
+    comment = sql.CharField()
+    created = sql.TimestampField()
 
 class Datum(Table):
-	thing_id = sql.ForeignKeyField(Thing, backref='data')
-	begin_revision = sql.IntegerField()
-	end_revision = sql.IntegerField()
-	key = sql.CharField()
-	value = sql.TextField() # data
-	datatype = sql.CharField() # 'string', 'reference', 'int', 'float', or 'date'
-	ordering = sql.IntegerField() # default null
+    thing_id = sql.ForeignKeyField(Thing, backref='data')
+    begin_revision = sql.IntegerField()
+    end_revision = sql.IntegerField()
+    key = sql.CharField()
+    value = sql.TextField() # data
+    datatype = sql.CharField() # 'string', 'reference', 'int', 'float', or 'date'
+    ordering = sql.IntegerField() # default null
 
 def init():
-	try:
-		db.create_tables([User,Thing,Version,Datum], safe=True)
-	except Exception as e: 
-		raise
+    try: db.create_tables([User,Thing,Version,Datum], safe=True)
+    except Exception as e: raise
--- settings.py
+++ settings.py
@@ -4,5 +4,4 @@ from peewee import *
 from playhouse.db_url import connect
 
 production_mode = os.environ.get('PRODUCTION_MODE', False)
-url = os.environ.get('DATABASE_URL', 'postgres://postgres:postgres@localhost:5432/renpan')
-db = connect(url)
+db = connect(os.environ.get('DATABASE_URL', 'postgres://postgres:postgres@localhost:5432/renpan'))
