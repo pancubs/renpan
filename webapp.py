64e0ae84ed4f2728bdaa6085796fbe0043459a63
Sonny Kothapally <me@sonnyksimon.com>
2019-04-13 01:42:13 -0400

    add styles

--- webapp.py
+++ webapp.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
 import flask
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path='')
 
 @app.route("/")
 def hello():
-	return "Hello, world!"
+    return flask.render_template('index.html',front=True,title="a good news organizer")
+
+@app.route('/css/<path:path>')
+def serve_css(path):
+    return flask.send_from_directory('css', path)
 
 if __name__ == '__main__': app.run()
