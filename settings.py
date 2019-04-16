d6dbaa905717714295c2e09c6432f100d0d701ed
Sonny Kothapally <me@sonnyksimon.com>
2019-04-16 19:07:04 -0400

    check heroku server os

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@ import os
 from peewee import *
 from playhouse.db_url import connect
 
-production_mode = os.environ.get('PRODUCTION_MODE', False)
+production_mode = bool(os.environ.get('PRODUCTION_MODE', False))
 db = connect(os.environ.get('DATABASE_URL', 'postgres://postgres:postgres@localhost:5432/renpan'))
--- webapp.py
+++ webapp.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 import flask
-import utils
+import utils as _utils
+import platform as _platform
+
+from settings import production_mode,db
 
 app = flask.Flask(__name__)
-_pyversion = utils.pyversion()
 
 @app.route("/")
 def hello():
-    #return flask.render_template('index.html',front=True,title="a good news organizer")
-    return flask.render_template('index.html',pyversion=_pyversion)
+    return flask.render_template('index.html',pyversion=_utils.pyversion(),distro=_platform.linux_distribution())
 
-if __name__ == '__main__': app.run()
+if __name__ == '__main__': app.run(debug=not(production_mode))
